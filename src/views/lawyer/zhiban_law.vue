9bef6d940629815acf36bc08587300b396298c66
徐泽鹏 <xuzepeng@jc-ai.com>
2020-04-11 10:40:47 +0800

    值班表

--- src/views/lawyer/zhiban_law.vue
+++ src/views/lawyer/zhiban_law.vue
@@ -1,35 +1,25 @@
 <template>
 	<div>
-		<div class="zaixian_top">
+		<div class="header-top">
 			<el-breadcrumb separator-class="el-icon-arrow-right">
-			  <el-breadcrumb-item :to="{ path: '/zhize' }">职责</el-breadcrumb-item>
-			  <el-breadcrumb-item>值班律师表</el-breadcrumb-item>
+				<el-breadcrumb-item :to="{ path: '/zhize' }">职责</el-breadcrumb-item>
+				<el-breadcrumb-item>值班律师表</el-breadcrumb-item>
 			</el-breadcrumb>
 		</div>
 		<div class="zhiban">
-			
+
 			<div>
-				<el-calendar v-model="value" id="calendar">
-				      <!-- 这里使用的是 2.5 slot 语法，对于新项目请使用 2.6 slot 语法-->
-				      <template
-				       slot="dateCell"
-				       slot-scope="{date, data}">
-				        <!--自定义内容-->
-				          <div>
-				             <div class="calendar-day">{{ data.day.split('-').slice(2).join('-') }}</div>
-				                <div v-for="(item,index) in datas" :key="index">
-				                   <!-- <div v-if="(item.months).indexOf(data.day.split('-').slice(1)[0])!=-1"> -->
-				                     <div v-if="(item.dutyDay).indexOf(data.day.split('-').slice(2).join('-'))!=-1">
-				                          <el-tooltip class="item" effect="dark" :content="item.lawyerName" placement="right">
-				                               <div class="is-selected">{{item.lawyerName}}</div>
-				                          </el-tooltip>
-				                       </div>
-				                    <div v-else></div>
-				                   <!-- </div> -->
-				               <!-- <div v-else></div> -->
-				             </div>
-				          </div>
-				      </template>
+				<el-calendar v-model="newData" id="calendar">
+					<!-- 这里使用的是 2.5 slot 语法，对于新项目请使用 2.6 slot 语法-->
+					<template
+									slot="dateCell"
+									slot-scope="{date, data}">
+						<!--自定义内容-->
+						<div class="calendar-day">{{ data.day.split('-').slice(2).join('-') }}</div>
+						<div class="calendar-name" v-for="(item, index) in dateList[data.day]" :key="index">
+							{{item}}
+						</div>
+					</template>
 				</el-calendar>
 			</div>
 			<div class="print">
@@ -37,46 +27,51 @@
 			</div>
 		</div>
 	</div>
-	
+
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        datas:'',
-		calendarData: [
-			{ months: ['09', '11'],days: ['15'],things: '看电影' },
-			{ months: ['10', '11'], days: ['02'],things: '去公园野炊' },
-			{ months: ['11'], days: ['02'],things: '看星星' },
-			{ months: ['11'], days: ['02'],things: '看月亮' }   
-		],
-      }
-    },
-	created() {
-		this.init()
-	},
-	methods:{
-		init(){
-			this.$axios.post(`/api/lawyer/getOnDutyList`, {
-				"token": sessionStorage.getItem("token"), //类型：String  必有字段  备注：token 用户身份标识
-				"yearMonth":"2019-06"  
-			}, {
-				headers: {
-					'Content-Type': 'application/json',
-					'Accept-Charset': 'utf-8'
-				}
-			}).then((respon) => {
-				if (respon) {
-					this.datas = respon.data.content.dataList
-				}
-			})
+	import {getDutyLawyerList} from '../../http/api.js'
+	import {formatDate} from '../../utils/date.js'
+	export default {
+		data() {
+			return {
+				newData: new Date(),
+				dateList: []
+			}
+		},
+		created() {
+			this.init();
+		},
+		methods:{
+			init(){
+				const year = this.newData.getFullYear(); // 获取年份
+				const month = this.newData.getMonth(); // 获取月份
+				const startDate = formatDate(new Date(year, month, 1), 'yyyy-MM-dd'); //获取显示月份的第一天
+				const endDate = formatDate(new Date(year, month + 1, 0), 'yyyy-MM-dd'); //获取显示月份的最好一天
+				const request = {token: sessionStorage.getItem("token"), startDate, endDate};
+				getDutyLawyerList(request).then(res => {
+					this.dateList = res.content.dataList.reduce((obj, val) => {
+						obj[val.dutyDate] = val.dutyList.map(item => {
+							return item.lawyerName
+						});
+						return obj;
+					}, {});
+				})
+			}
+		},
+		watch: {
+			newData: function (val) {
+				this.init();
+			}
 		}
 	}
-  }
 </script>
 
 <style lang="scss">
+	.header-top {
+		padding: 20px;
+	}
 	.zhiban{
 		width: 94%;
 		padding: 20px 3%;
@@ -97,4 +92,7 @@
 	// 	font-size: 28px;
 	// 	margin-bottom: 20px;
 	// }
+	.calendar-name {
+		font-size: 12px;
+	}
 </style>
